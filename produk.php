8ec473ff992a870d465aa099e373111b0ef9c7d5
dafabaelah <dafabaelah12@gmail.com>
2021-09-16 12:08:45 +0700

    OOP DASAR pada PHP #3 - Property dan Method

--- produk.php
+++ produk.php
@@ -3,12 +3,44 @@
 // Jualan Produk
 // Komik
 // Game
-calss Produk {
-    public $judul,
-            $penulis,
-            $harga;
+class Produk {
+    public $judul = "judul",
+            $penulis = "penulis",
+            $penerbit = "penerbit",
+            $harga = 0;
+    
+    public function getLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
 
 }
 
-$produk1 = new Produk();
-$produk2 = new Produk();
+
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
+
+// $produk2 = new Produk();
+// $produk2->judul = "Uncharted";
+// $produk2->tambahProperty = "hahaha";
+// var_dump($produk2);
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 300000;
+
+echo $produk3->getLabel();
+
+echo "<hr>";
+
+$produk4 = new Produk();
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil Druckmann";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = 250000;
+
+echo "Komik : ". $produk3->getLabel();
+echo "<br>";
+echo "Game : ". $produk4->getLabel();
