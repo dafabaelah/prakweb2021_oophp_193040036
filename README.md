fb609eb22901347f2c8c882388072e7fe79269db
dafabaelah <dafabaelah12@gmail.com>
2021-09-16 11:12:34 +0700

    tes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # prakweb2021_oophp_193040036
 Repository untuk tugas OOPHP mata kuliah Praktikum Web 2021
+
+@daffa akhdan fadhillah
---
+++ produk.php
@@ -0,0 +1,14 @@
+<?php
+
+// Jualan Produk
+// Komik
+// Game
+calss Produk {
+    public $judul,
+            $penulis,
+            $harga;
+
+}
+
+$produk1 = new Produk();
+$produk2 = new Produk();
