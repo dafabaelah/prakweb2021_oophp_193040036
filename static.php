44cdf5ada2a69bc5b0b1f67f6c8313cd835e191b
dafabaelah <dafabaelah12@gmail.com>
2021-09-23 13:27:08 +0700

    OOP DASAR pada PHP #11 - Static Keyword

--- static.php
+++ static.php
@@ -1,17 +1,36 @@
 <?php
 
-class ContohStatic {
+// class ContohStatic {
+//     public static $angka = 1;
+
+//     public static function hallo() {
+//         return "Halo ". self::$angka++. " kali.";
+//     }
+// }
+
+// echo ContohStatic::$angka;
+// echo "<br>";
+// echo ContohStatic::hallo();
+// echo "<hr>";
+// echo ContohStatic::hallo();
+
+class Contoh {
     public static $angka = 1;
 
-    public static function hallo() {
-        return "Halo ". self::$angka++. " kali.";
+    public function halo() {
+        return "Halo ".self::$angka++." Kali. <br>";
     }
 }
 
-echo ContohStatic::$angka;
-echo "<br>";
-echo ContohStatic::hallo();
-echo "<hr>";
-echo ContohStatic::hallo();
+$obj = new Contoh;
+echo $obj->halo();
+echo $obj->halo();
+echo $obj->halo();
 
+echo "<hr>";
+
+$obj2 = new Contoh;
+echo $obj2->halo();
+echo $obj2->halo();
+echo $obj2->halo();
 ?>
