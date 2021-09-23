35ec241988cbfc115d5b777e294dbeec5fb869e1
dafabaelah <dafabaelah12@gmail.com>
2021-09-23 13:39:43 +0700

    OOP DASAR pada PHP #12 - Constant

--- constant.php
+++ constant.php
@@ -1,7 +1,35 @@
 <?php
 
-define('Nama', 'Daffa Akhdan Fadhillah');
+// define('Nama', 'Daffa Akhdan Fadhillah');
 
-echo Nama;
+// echo Nama;
+
+// echo "<br>";
+
+// const UMUR = 20;
+// echo UMUR;
+
+// class Coba {
+//     const NAMA = 'sandhika';
+// }
+
+// echo Coba::NAMA;
+
+
+// echo __FILE__;
+
+// function coba() {
+//     return __FUNCTION__;
+// }
+
+// echo coba();
+
+
+class Coba {
+    public $kelas = __CLASS__;
+}
+
+$obj = new Coba;
+echo $obj->kelas;
 
 ?>
